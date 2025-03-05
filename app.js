fac1a622b7ad569a8c7759b8c88e768f52cff739
DamithaPerera <damitha@supplied.eu>
2025-03-05 08:37:50 +0530

    db

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
+mongoose.connect(`mongodb+srv://DamithaPerera:${process.env.DATABASE_PASSWORD}@payever.qigsu.mongodb.net/?retryWrites=true&w=majority&appName=payever`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error(err));
 
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
