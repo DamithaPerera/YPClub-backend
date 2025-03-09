549d0232bedacb08519324da0e3d547c46ed44c6
DamithaPerera <damitha@supplied.eu>
2025-03-09 08:48:55 +0530

    UUID as id and job post issue fixed

--- models/Job.js
+++ models/Job.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const JobSchema = new mongoose.Schema({
+    id: { type: String, default: uuidv4 },
     title: { type: String, required: true },
     description: String,
     skills: [String],
@@ -8,6 +10,8 @@ const JobSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now }
 });
 
-JobSchema.index({ title: "text", description: "text", skills: 1 });
+JobSchema.index({ title: "text", description: "text" });
+
+JobSchema.index({ skills: 1 });
 
 module.exports = mongoose.model('Job', JobSchema);
