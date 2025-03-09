c22d153dd51c4bc81d13845570a4d5aa2dcfab90
DamithaPerera <damitha@supplied.eu>
2025-03-09 19:02:34 +0530

    redis:

---
+++ redisClient.js
@@ -0,0 +1,13 @@
+const redis = require('redis');
+
+const client = redis.createClient({
+    url: 'redis://localhost:6379'
+});
+
+client.on('error', (err) => {
+    console.error('Redis error:', err);
+});
+
+client.connect().catch(console.error);
+
+module.exports = client;
