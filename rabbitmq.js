e26cdfd9e13b69131ef2061c61f1ba0852c25bae
DamithaPerera <damitha@supplied.eu>
2025-03-09 20:28:50 +0530

    workflow test

--- rabbitmq.js
+++ rabbitmq.js
@@ -7,7 +7,7 @@ const connectRabbitMQ = async () => {
         const connection = await amqp.connect(process.env.RABBITMQ_URL || 'amqp://localhost');
         channel = await connection.createChannel();
         await channel.assertQueue('applicationsQueue', { durable: true });
-        console.log('RabbitMQ connected and queue asserted.');
+        console.info('RabbitMQ connected and queue asserted.');
     } catch (error) {
         console.error('RabbitMQ connection error:', error);
     }
