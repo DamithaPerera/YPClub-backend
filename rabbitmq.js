e7a3eee213f7a5422e3f459830ad7cce81253dc1
DamithaPerera <damitha@supplied.eu>
2025-03-09 19:04:20 +0530

    rabbitMQ

---
+++ rabbitmq.js
@@ -0,0 +1,20 @@
+const amqp = require('amqplib');
+
+let channel;
+
+const connectRabbitMQ = async () => {
+    try {
+        const connection = await amqp.connect('amqp://localhost');
+        channel = await connection.createChannel();
+        await channel.assertQueue('applicationsQueue', { durable: true });
+        console.log('RabbitMQ connected and queue asserted.');
+    } catch (error) {
+        console.error('RabbitMQ connection error:', error);
+    }
+};
+
+connectRabbitMQ();
+
+const getChannel = () => channel;
+
+module.exports = { getChannel };
