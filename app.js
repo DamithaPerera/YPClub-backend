ff7cf4f04554dab01a0fe24cf8b7fbc9e5efff91
DamithaPerera <damitha@supplied.eu>
2025-03-09 08:35:59 +0530

    issue fixed

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const authenticate = (req, res, next) => {
     const token = req.headers['authorization'];
     if (!token)
         return res.status(401).json({ message: 'Unauthorized: No token provided' });
-    jwt.verify(token, `${process.env.JWT}`, (err, decoded) => {
+    jwt.verify(token, `${process.env.SECRET}`, (err, decoded) => {
         if (err)
             return res.status(401).json({ message: 'Unauthorized: Invalid token' });
         req.user = decoded;
